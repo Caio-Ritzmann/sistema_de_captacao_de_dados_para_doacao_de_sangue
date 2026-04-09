fc04ce0ad68901804f671ce336281e88d6655f88
caio <caio_pacheco@estudante.sesisenai.org.br>
2026-04-09 09:35:08 -0300

    add -let's

--- script/script.js
+++ script/script.js
@@ -2,3 +2,18 @@ let listaPessoas = [];
 
 const formulario = document.getElementById("formulario");
 const mensagem = document.getElementById("mensagem");
+
+formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let idade = document.getElementById("idade").value;
+    let peso = document.getElementById("peso").value;
+    let tipoSanguineo = document.getElementById("tipoSanguineo").value;
+    let telefone = document.getElementById("telefone").value;
+    let cidade = document.getElementById("cidade").value;
+    let estado = document.getElementById("estado").value;
+
+
+});
